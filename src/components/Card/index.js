69c9f61e9fa051ac308ad8f063dd6836fed4bd37
Jesiel Faria <fariajesiel@gmail.com>
2021-10-05 09:10:07 -0300

    adicionando componente cards e estilos

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Title } from './styles';
+import { Container, ContainerBody, Card, Text, BoxText, Image } from './styles';
+import ahri from '../../images/AhriFliperama.jpg'
+import ezreal from '../../images/EzrealFliperama.jpg'
+import cait from '../../images/CaitFliperama.jpg'
+
+
 export default class App extends React.Component {
-  
+
   render() {
     return (
-      <>
-        <Title>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-        </Title>
-      </>
+      <Container>
+        <Text fontSize={40}>Cards</Text>
+        <ContainerBody>
+
+          <Card>
+            <Image src={ezreal} />
+            <BoxText>
+              <Text fontSize={20}>Nome: Ezreal</Text>
+              <Text fontSize={12}>Sobrenome: Fliperama</Text>
+            </BoxText>
+          </Card>
+
+          <Card>
+            <Image src={ahri} />
+            <BoxText>
+              <Text fontSize={20}>Nome: Ahri</Text>
+              <Text fontSize={12}>Sobrenome: Fliperama</Text>
+            </BoxText>
+          </Card>
+
+          <Card>
+            <Image src={cait} />
+            <BoxText>
+              <Text fontSize={20}>Nome: Caitlyn</Text>
+              <Text fontSize={12}>Sobrenome: Fliperama</Text>
+            </BoxText>
+          </Card>
+
+        </ContainerBody>
+      </Container>
     )
   }
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,5 +1,41 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #00FFFF;
+export const Container = styled.h1`
+  background-color: #00ffff;
+`;
+
+export const ContainerBody = styled.h1`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Text = styled.h1`
+  color: #000;
+  text-shadow: 5px 2px 4px gray;
+  font-size: ${props => `${props.fontSize}px`};
+  padding-left: 10px;
+`;
+
+export const Card = styled.div`
+  /* display: flex; */
+  margin: 1rem;
+  width: 20rem;
+  height: 30rem;
+  border-radius: 15px;
+  justify-content: space-around;
+  box-shadow: 6px 4px 8px gray;
+  background-color: #fff;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 50%;
+  border-top-right-radius: 15px;
+  border-top-left-radius: 15px;
+  /* padding: 1rem; */
+`;
+
+export const BoxText = styled.div`
+  width: 18rem;
+  padding: 1rem;
 `;
